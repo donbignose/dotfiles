f87280efe64c33affb613f4e1def1fd829ba1e53
donbignose <stamerra17@gmail.com>
2023-05-20 17:39:50 +0200

    Add venv-selector

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local wk = require("which-key")
+
+wk.register({
+  v = {
+    name = "venv",
+  },
+}, { prefix = "<leader>" })
