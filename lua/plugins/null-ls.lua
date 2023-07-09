4629ed3b45a33757363f1904182686b0834269e5
donbignose <stamerra17@gmail.com>
2023-07-09 16:51:39 +0200

    Add python extra and mason-null-ls

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {
+        nls.builtins.diagnostics.flake8.with({ extra_args = { "--extend-ignore=E203", "--max-line-length", "88" } }),
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.autoflake,
+      },
+    }
+  end,
+}
