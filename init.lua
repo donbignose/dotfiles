61ed33fc4d217dc0d1e3ff835fb2a0589edc6b88
donbignose <stamerra17@gmail.com>
2023-07-15 18:02:34 +0200

    Update nvim

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-local wk = require("which-key")
-
-wk.register({
-  v = {
-    name = "venv",
-  },
-}, { prefix = "<leader>" })
