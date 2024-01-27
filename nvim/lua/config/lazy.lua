7ca5e3f6249974eafffa5ed0cfed5cacb942b627
donbignose <stamerra17@gmail.com>
2024-01-27 17:11:52 +0100

    update dotfiles

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.lang.python" },
