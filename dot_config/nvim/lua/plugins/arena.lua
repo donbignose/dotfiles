67833fad07bfb41ad9c98d4d96124977245faf5e
donbignose <stamerra17@gmail.com>
2024-10-10 12:30:54 +0200

    fix(nvim): remove arena and mason-null

--- dot_config/nvim/lua/plugins/arena.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "dzfrias/arena.nvim",
-  event = "BufWinEnter",
-  -- Calls `.setup()` automatically
-  config = true,
-  keys = { { "<leader>ba", "<cmd>ArenaToggle<cr>", desc = "Arena" } },
-}
